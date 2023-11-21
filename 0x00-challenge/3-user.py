ade07f41c28cf35461393263c41133a066674aa2
AmazedWonder <giftedwonda@gmail.com>
2023-11-21 05:36:58 +0100

    fixed error, User password

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -50,12 +50,13 @@ class User():
         - `False` if `pwd` is not a string
         - `False` if `__password` is `None`
         - Compare `__password` and the MD5 value of `pwd`
+        - Fixed error .upper to .lower line 59
         """
         if pwd is None or type(pwd) is not str:
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
